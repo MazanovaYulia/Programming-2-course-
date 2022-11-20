49e9ab64966e2f91f8a3cc556a3fb9a879100ca3
Aulia <94062075+MazanovaYulia@users.noreply.github.com>
2022-11-21 00:53:36 +0700

    Update Form1.cs

--- LAB1/Form1.cs
+++ LAB1/Form1.cs
@@ -1,99 +1,251 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
-using AngouriMath;
-using AngouriMath.Extensions;
 
-namespace WindowsFormsApp1
+namespace Dihtomia
 {
-
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            Chart.Series[0].Points.Clear();
+            Chart.Series[1].Points.Clear();
+
+            string Function = FunctionBox.Text.Replace("x", "(x)");
+            bool StartStatus = CheckDouble(StartABox.Text, "a");
+            bool EndStatus = CheckDouble(EndBBox.Text, "b");
+            bool AccuracyStatus = CheckInt(AccuracyBox.Text, "e");
+            bool FormulaStatus = true;
+
+            MathParser Parser = new MathParser();
+            try
+            {
+                List<double> TestPoint = GetPoint(Parser, Function, 1);
+            }
+            catch
+            {
+                MessageBox.Show($"Ошибка в формуле");
+                FormulaStatus = false;
+            }
+
+
+            if (StartStatus && EndStatus && AccuracyStatus && FormulaStatus)
+            {
+                double Start = Double.Parse(StartABox.Text);
+                double End = Double.Parse(EndBBox.Text);
+                int Accuracy = Int32.Parse(AccuracyBox.Text);
+                double AmountPoints = 10000;
+
+                if (End > Start)
+                {
+                    try
+                    {
+                        List<List<double>> Points = GetPoints(Parser, Function, Start, End, AmountPoints);
+
+                        List<double> MinumumPoint = GetMinimumPoint(Parser, Function, Start, End, Accuracy);
+                        double Step = (End - Start) / AmountPoints;
+                        List<List<double>> AllMinimumPoints = GetMinimumPoints(Points, MinumumPoint, Step);
+
+                        DrawChart(Points, AllMinimumPoints);
+
+                        if (MinumumPoint[1] is double.NaN)
+                        {
+                            ResultLabel.Text = $"Локальный минимум не найден";
+                        }
+                        else
+                        {
+                            ResultLabel.Text = $"Локальный минимум: ( {MinumumPoint[0]}, {TruncateDecimal(MinumumPoint[1], Accuracy)} )";
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show($"Слишком сложная формула");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Конец интервала не может быть меньше начала интервала");
+                }
+            }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        static double TruncateDecimal(double value, int precision)
         {
+            double step = (double)Math.Pow(10, precision);
+            double tmp = Math.Truncate(step * value);
+            return tmp / step;
+        }
 
-            Entity formula = Convert.ToString(textBox1.Text);
-            var convFormula = formula.Differentiate("x").Simplify();
-            chart1.Series[0].Points.Clear();
-            chart1.Series[1].Points.Clear();
-            chart1.Series[2].Points.Clear();
-            chart1.Series[3].Points.Clear();
-
-            int GetDecimalDigitsCount(double number)
+        void DrawChart(List<List<double>> Points, List<List<double>> MinimumPoints)
+        {
+            foreach (List<double> Point in Points)
             {
-                string[] str = number.ToString(new System.Globalization.NumberFormatInfo() { NumberDecimalSeparator = "." }).Split('.');
-                return str.Length == 2 ? str[1].Length : 0;
+                if (Point[0] > 99999999 || Point[0] < -99999999 || Point[1] > 99999999 || Point[1] < -99999999 || Point[1] is double.NaN)
+                {
+                    continue;
+                }
+                else
+                {
+                    Chart.Series[0].Points.AddXY(Point[0], Point[1]);
+                }
+            }
+            foreach (List<double> MinimumPoint in MinimumPoints)
+            {
+                Chart.Series[1].Points.AddXY(MinimumPoint[0], MinimumPoint[1]);
             }
 
-            chart1.Series[0].XValueType = ChartValueType.Double;
+            Chart.Series[0].Points.ResumeUpdates();
+            Chart.Series[1].Points.ResumeUpdates();
+        }
 
+        List<double> GetPoint(MathParser Parser, string Function, double X)
+        {
+            List<double> Point = new List<double>();
+            string NowFunction = String.Copy(Function).ToLower();
+            NowFunction = NowFunction.Replace("x", X.ToString());
 
-            chart1.ChartAreas[0].AxisX.IntervalOffsetType = DateTimeIntervalType.Number;
-            //chart1.ChartAreas[0].AxisX.Interval = 11;
+            double Y = Parser.Parse(NowFunction, true);
 
-            double minX = Convert.ToDouble(textBox5.Text);
-            double maxX = Convert.ToDouble(textBox6.Text);
-            for (double i = minX;  i < maxX + 1; i += 1) {
-             
-                var y = (double)formula.Substitute("x", i).EvalNumerical();
-                chart1.Series[0].Points.AddXY(i, y);
+            Point.Add(X);
+            Point.Add(Math.Round(Y, 8));
 
-                var addx1 = Convert.ToDouble(textBox2.Text);
-                var addy1 = formula.Substitute("x", addx1).EvalNumerical().Stringize();
+            return Point;
+        }
 
-                chart1.Series[2].Points.AddXY(addx1, 0);
-                chart1.Series[2].Points.AddXY(addx1, addy1);
+        List<List<double>> GetMinimumPoints(List<List<double>> Points, List<double> MinimumPoint, double Step)
+        {
+            List<List<double>> MinimumPoints = new List<List<double>>();
 
-                var addx2 = Convert.ToDouble(textBox3.Text);
-                var addy2 = formula.Substitute("x", addx2).EvalNumerical().Stringize();
-                chart1.Series[3].Points.AddXY(addx2, 0);
-                chart1.Series[3].Points.AddXY(addx2, addy2);
+            double LastMinimumPointX = -999999;
+            foreach (List<double> Point in Points)
+            {
+                double DiffX = Math.Abs(Point[0] - LastMinimumPointX);
+                double DiffY = Math.Abs(Point[1] - MinimumPoint[1]);
+                if (DiffY < 0.001)
+                {
+                    LastMinimumPointX = Point[0];
+                    MinimumPoints.Add(Point);
+                }
             }
-            var a = Convert.ToDouble(textBox2.Text);
-            var b = Convert.ToDouble(textBox3.Text);
-            double c = 0;
-            var convA = (double)convFormula.Substitute("x", a).EvalNumerical();
-            var convB = (double)convFormula.Substitute("x", b).EvalNumerical();
-            var precision = Convert.ToInt32(textBox7.Text);
-            if (convA * convB < 0)
+
+            return MinimumPoints;
+        }
+
+        List<double> GetMinimumPoint(MathParser Parser, string Function, double Start, double End, int Accuracy)
+        {
+            double Y = 0;
+            double X = 0;
+            double Precision = 1 / Math.Pow(10, Accuracy);
+            double Length = Precision / 2 - Precision / 4;
+
+            do
             {
-            while (GetDecimalDigitsCount(c) != precision)
-            {
-                chart1.Series[1].Points.Clear();
-                c = (a + b)/2;
-                var Fa = (double)convFormula.Substitute("x", a).EvalNumerical();
-                var Fb = (double)convFormula.Substitute("x", b).EvalNumerical();
-                var Fc = (double)convFormula.Substitute("x", c).EvalNumerical();
-                var Fcc = (double)formula.Substitute("x", c).EvalNumerical();
-                if(Fc * Fa < 0)
+                X = (Start + End) / 2;
+                double StartX = X - Length;
+                double EndX = X + Length;
+                double StartY = GetPoint(Parser, Function, StartX)[1];
+                double EndY = GetPoint(Parser, Function, EndX)[1];
+
+                if (StartY is double.NaN)
                 {
-                    b = c;
-                } else
-                {
-                    a = c;
+                    StartY = 0;
                 }
 
-                chart1.Series[1].Points.AddXY(c,Fcc);
-                textBox4.Text = Convert.ToString(c);
-            }
-            } else
+                if (EndY is double.NaN)
+                {
+                    EndY = 0;
+                }
+
+                Y = GetPoint(Parser, Function, X)[1];
+
+                if (StartY <= EndY)
+                {
+                    End = EndX;
+                }
+                else if (StartY > EndY)
+                {
+                    Start = StartX;
+                }
+            } while (Math.Abs(Start - End) >= Precision);
+
+            List<double> MinimumPoint = new List<double>();
+            MinimumPoint.Add(X);
+            MinimumPoint.Add(Y);
+
+            return MinimumPoint;
+        }
+
+        List<List<double>> GetPoints(MathParser Parser, string Function, double Start, double End, double AmountPoints)
+        {
+            List<List<double>> Points = new List<List<double>>();
+            List<double> XPoints = new List<double>();
+            double X = Start;
+            double Step = (End - Start) / AmountPoints;
+
+            while (X <= End)
             {
-                chart1.Series[1].Points.Clear();
-                MessageBox.Show("Корня в этом интервале нет");
+                List<double> Point = GetPoint(Parser, Function, X);
+                X = Math.Round(X + Step, 4);
+                if (Point[1] is double.NaN)
+                {
+                    continue;
+                }
+                Points.Add(Point);
+                XPoints.Add(X);
             }
-        }  
+
+            if (!XPoints.Contains(End))
+            {
+                List<double> Point = GetPoint(Parser, Function, End);
+                Points.Add(Point);
+                XPoints.Add(End);
+            }
+
+            return Points;
+        }
+
+        bool CheckDouble(string DoubleLine, string DoubleName)
+        {
+            if (Double.TryParse(DoubleLine, out double DoubleNumber))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show($"Неподходящий формат: {DoubleName}");
+                return false;
+            }
+        }
+
+        bool CheckInt(string IntLine, string IntName)
+        {
+            if (Int32.TryParse(IntLine, out int IntNumber))
+            {
+                if (IntNumber >= 1 && IntNumber <= 7)
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("Точность должна быть от 1 до 7 знаков после запятой.");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Неподходящий формат: {IntName}");
+                return false;
+            }
+        }
+
+        private void Chart_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
